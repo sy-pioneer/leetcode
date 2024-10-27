c4f071fd16d96c0d8780dd82d4e68d83899c48e5
sy-pioneer <zhoushiya0828@163.com>
2024-10-27 20:28:57 +0800

    C++节点

---
+++ ROS2/CHAPT2_WS/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zsy/sy_git/ROS2/CHAPT2_WS
+# Build directory: /home/zsy/sy_git/ROS2/CHAPT2_WS
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- ROS2/CHAPT2_WS/ros2_cpp_node.cpp
+++ ROS2/CHAPT2_WS/ros2_cpp_node.cpp
@@ -1,13 +1,12 @@
-#include "iostream"
+#include "rclcpp/rclcpp.hpp"
+
 int main(int argc, char** argv)
 {
-    std::cout << "参数数量=" << argc << std::endl; //argc表示参数数量，
-    std::cout << "程序名字=" << argv[0] << std::endl; //argv表示输入参数，参数名字，argv[0]第一个参数默认是程序的名字
-    // g++ 文件名；编译之后会生成a.out的可执行文件，这时候在终端中执行./a.out，./a.out会作为argv的第一个参数，argc的值为1
-    //执行./a.out --help；这时argc表示参数数量的值为2，./a.out作为第一个参数，--help作为第二个参数输入
-    std::string arg1 = argv[1];
-    if (arg1 == "--help"){
-        std::cout << "这里是程序帮助，但是这个程序什么用都没有" << std::endl;
-    }
+    rclcpp::init(argc, argv);//初始化传入参数argc,argv;
+    //作用是ros2运行程序这个的时候，可执行程序后面加一些参数(修改节点的名字），会被rclcpp读取和解析
+    auto node = std::make_shared<rclcpp::Node>("cpp_node");
+    RCLCPP_INFO(node->get_logger(), "你好C++节点！");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }

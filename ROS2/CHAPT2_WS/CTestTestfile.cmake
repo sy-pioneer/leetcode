1f516abba56544a433933433f444c6927c3be2c1
sy-pioneer <zhoushiya0828@163.com>
2024-10-27 21:21:07 +0800

    python功能包

--- ROS2/CHAPT2_WS/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zsy/sy_git/ROS2/CHAPT2_WS
-# Build directory: /home/zsy/sy_git/ROS2/CHAPT2_WS
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

4935c4cc225d92be8674f55fa7d26ddef32108cc
sy-pioneer <zhoushiya0828@163.com>
2025-01-04 22:05:49 +0800

    20250104

---
+++ ROS2/chapt4/chapt4_ws/install/chapt4_interfaces/include/chapt4_interfaces/chapt4_interfaces/srv/face_detector.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef CHAPT4_INTERFACES__SRV__FACE_DETECTOR_HPP_
+#define CHAPT4_INTERFACES__SRV__FACE_DETECTOR_HPP_
+
+#include "chapt4_interfaces/srv/detail/face_detector__struct.hpp"
+#include "chapt4_interfaces/srv/detail/face_detector__builder.hpp"
+#include "chapt4_interfaces/srv/detail/face_detector__traits.hpp"
+
+#endif  // CHAPT4_INTERFACES__SRV__FACE_DETECTOR_HPP_
